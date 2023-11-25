5ba2f7785845a81f4837be6cb3e9dc13daf31e08
ovandoa26@gmail.com <ovandoa26@gmail.com>
2023-11-25 00:27:00 -0500

    adding immutable collection

--- src/main/java/com/springbootlearning/learningspringboot3/HomeController.java
+++ src/main/java/com/springbootlearning/learningspringboot3/HomeController.java
@@ -1,17 +1,23 @@
 package com.springbootlearning.learningspringboot3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 // communicates this class as controller
 @Controller
 public class HomeController {
+    record Video(String name) {}
+    List<Video> videos = List.of(
+            new Video("Need HELP with your Spring Boot 3 App?"),
+            new Video("Don't do THIS to your own CODE!"),
+            new Video("SECRETS to fix BROKEN CODE!"));
 
-    // maps call with HTTP GET method
     @GetMapping("/")
-    public String index() {
-
-        // name of template to render
+    public String index(Model model) {
+        model.addAttribute("videos", videos);
         return "index";
     }
 }
